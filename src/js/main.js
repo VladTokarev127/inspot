06ccd1b7c0cd0a6a78f88e0e10d12f18e3869138
Vladislav Tokarev <vladtokarev127@gmail.com>
2023-01-25 17:17:58 +0500

    contacts done

--- src/js/main.js
+++ src/js/main.js
@@ -33,13 +33,29 @@ $(function() {
 		setHeader();
 	});
 
-	$('.vacancy__top').click(function(e) {
+	$('[data-accordion-trigger]').click(function(e) {
 		e.preventDefault();
 		$(this)
 			.toggleClass('is-active')
-			.next()
+			.next('[data-accordion-target]')
 			.slideToggle(700);
 	});
+
+	$('[data-tab-content]').hide();
+	$('[data-tab-content]:nth-child(1)').show();
+	$(document).on('click','[data-tab]:not(.is-active)',function(e) {
+		e.preventDefault();
+		let index = $(this).index();
+		let parent = $(this).parents('[data-tabs]');
+		let contents = parent.next('[data-tab-contents]');
+		let currentContent = contents.find('[data-tab-content]:eq('+ index +')');
+		parent.find('[data-tab]').removeClass('is-active');
+		contents.find('[data-tab-content]').removeClass('is-active').fadeOut(300);
+		setTimeout(() => {
+			currentContent.fadeIn(300).addClass('is-active');
+		}, 300)
+		$(this).addClass('is-active');
+	})
 
 	const swiper = new Swiper('.swiper', {
 		autoHeight: false,
